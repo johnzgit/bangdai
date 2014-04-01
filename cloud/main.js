0bf55fb75e06afe7aefba3f5743acb0c6e7a62d0
qq312384485 <312384485@qq.com>
2014-04-01 22:39:32 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,9 @@
-require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+});
+AV.Cloud.define("getNeeds", function (request, response) {
+    var query = new AV.Query("Needs");
+    query.equalTo("par",request.params.par)
 });
